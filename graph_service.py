78357c94c30fbc666e3040fc7737e3d9fb7cf9e7
Mouse Reeve <mouse.reeve@gmail.com>
2015-12-03 22:51:03 -0800

    Filter entity lists

--- graph_service.py
+++ graph_service.py
@@ -42,10 +42,13 @@ class GraphService(object):
 
 
     @serialize
-    def get_all(self, label):
+    def get_all(self, label, with_connection_label=None):
         ''' load all nodes with a given label '''
-        data = self.query('MATCH (n:%s) RETURN n' % label)
-        return data
+        query = 'MATCH (n:%s) ' % label
+        if with_connection_label:
+            query += ' -- (m:%s) ' % with_connection_label
+        query += 'RETURN DISTINCT n'
+        return self.query(query)
 
 
     @serialize
@@ -59,35 +62,47 @@ class GraphService(object):
     @serialize
     def random(self):
         ''' select one random node '''
-        node = self.query('MATCH n RETURN n, rand() as random ORDER BY random limit 1')
+        node = self.query('MATCH n RETURN n, rand() as random ORDER BY random LIMIT 1')
         return node
 
 
     @serialize
     def search(self, term):
         ''' match a search term '''
-        data = self.query('match n where n.identifier =~ {term} or ' \
-                          'n.alternate_names =~ {term} or ' \
-                          'n.content =~ {term} return n', term='(?i).*%s.*' % term)
+        data = self.query('MATCH n WHERE n.identifier =~ {term} OR ' \
+                          'n.alternate_names =~ {term} OR ' \
+                          'n.content =~ {term} RETURN n', term='(?i).*%s.*' % term)
         return data
 
 
     @serialize
-    def related(self, uid, label, n=2, limit=5):
+    def related(self, uid, label, n=2, LIMIT=5):
         ''' find similar items, based on nth degree relationships '''
-        query = 'match (m)-[r*%d]-(n:%s) where m.uid = {uid} ' \
-                'return distinct n, count(r) order by count(r) desc ' \
-                'limit %d' % (n, label, limit)
+        query = 'MATCH (m)-[r*%d]-(n:%s) WHERE m.uid = {uid} ' \
+                'RETURN DISTINCT n, count(r) ORDER BY count(r) desc ' \
+                'LIMIT %d' % (n, label, LIMIT)
         return self.query(query, uid=uid)
 
 
     @serialize
     def others_of_type(self, label, uid, exclude):
         ''' get all <blank> related to item <blank> '''
-        query = 'match (n:%s)--(m) ' \
-                'where m.uid = {uid} ' \
-                'and not n.uid = {exclude} ' \
-                'return n limit 5' % (label)
+        query = 'MATCH (n:%s)--(m) ' \
+                'WHERE m.uid = {uid} ' \
+                'AND NOT n.uid = {exclude} ' \
+                'RETURN n LIMIT 5' % (label)
         return self.query(query, uid=uid, exclude=exclude)
 
 
+    @serialize
+    def get_filtered(self, label, item1, item2, operator):
+        ''' items that connect to nodes '''
+        query = 'MATCH (n:%s)--m, p ' \
+                'WHERE m.uid={item1} AND p.uid={item2} ' \
+                'AND ' % label
+
+        if operator == 'not':
+            query += 'NOT '
+
+        query += '(n)--(p) RETURN n'
+        return self.query(query, item1=item1, item2=item2)
--- grimoire.py
+++ grimoire.py
@@ -8,6 +8,7 @@ import re
 app = Flask(__name__)
 graph = GraphService()
 
+entities = ['angel', 'demon', 'olympian_spirit', 'fairy', 'aerial_spirit']
 
 # ----- routes
 @app.route('/', methods=['GET'])
@@ -85,17 +86,14 @@ def item(label, uid):
     rels = data['relationships']
 
     rel_exclusions = []
-    prop_exclusions = []
     template = 'item.html'
 
-    entities = ['angel', 'demon', 'olympian_spirit', 'fairy', 'aerial_spirit']
     if label == 'fairy':
         # removes duplication of two-way sister relationships
         rels = [r for r in rels if r['type'] != 'has_sister' or r['end']['id'] != node['id']]
 
     if label == 'grimoire':
         template = 'grimoire.html'
-        prop_exclusions = ['year', 'decade', 'century']
         rel_exclusions = ['lists', 'has']
 
         data['date'] = grimoire_date(node['properties'])
@@ -117,11 +115,10 @@ def item(label, uid):
 
     data['relationships'] = [r for r in rels if not r['type'] in rel_exclusions]
     data['id'] = node['id']
-    data['properties'] = {k:v for k, v in node['properties'].items() if not k in prop_exclusions}
+    data['properties'] = {k:v for k, v in node['properties'].items() if
+                          not k in ['year', 'decade', 'century']}
     data['has_details'] = len([k for k in data['properties'].keys()
                                if not k in ['uid', 'content', 'identifier']]) > 0
-
-    title = '%s (%s)' % (node['properties']['identifier'], capitalize_filter(format_filter(label)))
 
     sidebar = []
     related = graph.related(uid, label)
@@ -130,28 +127,40 @@ def item(label, uid):
         sidebar = [{'title': 'Similar %s' % pluralize(capitalize_filter(label)),
                     'data': related['nodes']}]
 
-    # other items of this type related to some item
+    sidebar += get_others(data['relationships'], node)
+
+    title = '%s (%s)' % (node['properties']['identifier'], capitalize_filter(format_filter(label)))
+
+    return render_template(template, data=data, title=title, label=label, sidebar=sidebar)
+
+
+def get_others(rels, node):
+    '''
+    other items of the node's type related to something it is related to.
+    For example: "Other editions by the editor Joseph H. Peterson"
+    '''
+    label = node['label']
     others = []
-    for rel in data['relationships']:
+    for rel in rels:
         start = rel['start']
         end = rel['end']
-        # different types of data
+
+        # must be different types of data, and not entities in a grimoire
         if start['label'] != end['label'] and \
                 (end['label'] != 'demon' and start['label'] != 'grimoire'):
+            # other = "editions"
             other = start if start['label'] != label else end
-            others.append(other)
             other_items = graph.others_of_type(label,
                                                other['properties']['uid'],
                                                node['properties']['uid'])
             if other_items['nodes']:
-                sidebar.append({
+                others.append({
                     'title': 'Other %s related to the %s %s' %
                              (pluralize(label), format_filter(other['label']),
                               other['properties']['identifier']),
                     'data': other_items['nodes']
                 })
-
-    return render_template(template, data=data, title=title, label=label, sidebar=sidebar)
+    return others
 
 
 @app.route('/<label>', methods=['GET'])
@@ -161,9 +170,42 @@ def category(label):
     if not graph.validate_label(label):
         labels = graph.get_labels()
         return render_template('label-404.html', labels=labels)
-    data = graph.get_all(label)
+
+    filtered = None
+    try:
+        item1 = sanitize(request.values['i'])
+        item2 = sanitize(request.values['j'])
+        operator = sanitize(request.values['op'])
+        if not operator in ['and', 'not']:
+            raise BadRequestKeyError
+    except (KeyError, BadRequestKeyError):
+        data = graph.get_all(label)
+    else:
+        data = graph.get_filtered(label, item1, item2, operator)
+        filtered = {
+            'operator': operator,
+            'item1': graph.get_node(item1)['nodes'][0],
+            'item2': graph.get_node(item2)['nodes'][0]
+        }
+
+    items = {}
+    for node in data['nodes']:
+        letter = node['properties']['identifier'][0].upper()
+        items[letter] = [node] if not letter in items else items[letter] + [node]
+
+    template = 'list.html'
     title = 'List of %s' % capitalize_filter(format_filter(label))
-    return render_template('list.html', data=data, title=title, label=label)
+
+    grimoires = []
+    if label in entities:
+        template = 'entity-list.html'
+        grimoires = graph.get_all('grimoire', with_connection_label=label)['nodes']
+        if len(grimoires) < 2:
+            grimoires = None
+
+    return render_template(template, items=items,
+                           title=title, label=label,
+                           grimoires=grimoires, filtered=filtered)
 
 
 # ----- filters
