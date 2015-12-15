6833d8c05005ee8102cd404f7fb10982fd1b52f3
Mouse Reeve <mouse.reeve@gmail.com>
2015-12-15 11:06:25 -0800

    config

--- grimoire/__init__.py
+++ grimoire/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from grimoire.graph_service import GraphService
 
 app = Flask(__name__)
+app.config.from_object('config')
+
 graph = GraphService()
 entities = ['angel', 'demon', 'olympian_spirit', 'fairy', 'aerial_spirit']
 
