64906875b6a3e8a8b4730c3f38df896acc91c936
Mouse Reeve <mouse.reeve@gmail.com>
2015-12-15 11:36:57 -0800

    Minor formatting fixes for config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+''' App settings '''
+
 # Statement for enabling the development environment
 DEBUG = True
 
@@ -5,14 +7,11 @@ DEBUG = True
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Application threads. A common general assumption is
-# using 2 per available processor cores - to handle
-# incoming requests using one and performing background
-# operations using the other.
+# Application threads.
 THREADS_PER_PAGE = 2
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
-CSRF_ENABLED     = True
+CSRF_ENABLED = True
 
 # Use a secure, unique and absolutely secret key for
 # signing the data.
