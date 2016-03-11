88839ecaae49c19313c9fa7d2474d474c9c19021
Mouse Reeve <mousereeve@riseup.net>
2016-03-10 17:18:16 -0800

    Add line breaks to content

--- grimoire/helpers.py
+++ grimoire/helpers.py
@@ -115,3 +115,7 @@ def unthe(text):
 
     return '%s %s' % (pieces[1], pieces[0])
 
+@app.template_filter('breaker')
+def breaker(text):
+    ''' replace linebreaks with br tags '''
+    return text.replace('\n', '<br>')
