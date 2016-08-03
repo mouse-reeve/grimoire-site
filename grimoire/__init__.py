ccbc6c75c287c94371ca902bd93ab9b6bfd85fce
Mouse Reeve <mousereeve@riseup.net>
2016-08-03 12:26:54 -0400

    remove temporospatial models

--- grimoire/__init__.py
+++ grimoire/__init__.py
@@ -7,17 +7,12 @@ from grimoire.graph_service import GraphService
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config.from_object('config')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['PSQL_URI']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 graph = GraphService()
 entities = graph.get_entity_labels()
 date_params = graph.date_params
 
 templates = {}
-
-import grimoire.models as temporospatial
-temporospatial.db.init_app(app)
 
 import grimoire.views
 import grimoire.item_views
