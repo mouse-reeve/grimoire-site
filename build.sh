4533eb39f1dc072756d26bb75b79cae1c63b8a11
Mouse Reeve <mousereeve@riseup.net>
2017-02-24 11:57:31 -0800

    Correct file path to javascript

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ source bin/activate
 python build_static.py
 
 cp -r grimoire/static _site/
-uglifyjs grimoire/static/temporospatial.js > _site/static/temporospatial.js
+uglifyjs grimoire/static/js/temporospatial.js > _site/static/js/temporospatial.js
 cssnano < grimoire/static/styles/app.css > _site/static/styles/app.css
